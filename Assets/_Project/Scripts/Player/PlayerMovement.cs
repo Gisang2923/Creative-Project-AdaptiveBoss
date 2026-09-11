abddd565292df0a23112ca0ca6ced2ae18a6a760
Gisang2923 <gisang1208@gmail.com>
2026-09-11 21:22:56 +0900

    feat: implement player jump

--- Assets/_Project/Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.15f;
+    [SerializeField] private LayerMask groundLayer;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
@@ -18,11 +22,38 @@ public class PlayerMovement : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Jump Input");
+
+            Debug.Log("Grounded: " + IsGrounded());
+
+            if (IsGrounded())
+            {
+                rb.linearVelocity = new Vector2(
+                    rb.linearVelocity.x,
+                    jumpForce
+                );
+            }
+        }
+    }
+
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(
             moveInput.x * moveSpeed,
             rb.linearVelocity.y
+        );
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
         );
     }
 }
