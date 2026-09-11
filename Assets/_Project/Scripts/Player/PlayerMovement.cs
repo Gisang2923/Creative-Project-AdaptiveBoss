c49df7c682dd27b71f6fe4afed285df81fcbd497
Gisang2923 <gisang1208@gmail.com>
2026-09-11 20:59:09 +0900

    feat: implement basic player movement

---
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(
+            moveInput.x * moveSpeed,
+            rb.linearVelocity.y
+        );
+    }
+}
